4be78a8d7331747cf3e8697d3c78fbc5d0462733
danielhingar <danielhingar3397@gmail.com>
2019-03-11 19:32:43 +0100

    Update SponsorshipRepository.java

--- src/main/java/repositories/SponsorshipRepository.java
+++ src/main/java/repositories/SponsorshipRepository.java
@@ -12,6 +12,14 @@ import domain.Sponsorship;
 @Repository
 public interface SponsorshipRepository extends JpaRepository<Sponsorship, Integer> {
 
+	//query a.1
+	@Query("select 1.0*count(s)/(select count(ss) from Sponsorship ss ) from Sponsorship s where s.isActive=true")
+	Double ratioActiveSponsorship();
+
+	//query a.2.1
+	@Query("select avg(1.0 * (select count(*) from Sponsorship s where s.sponsor = sp.id)) from Sponsor sp")
+	Double avgSponsorshipPorSponsor();
+
 	//Other queries-----------------------------------------------------------------
 
 	@Query("select s from Sponsorship s where s.sponsor.id = ?1")
@@ -19,9 +27,6 @@ public interface SponsorshipRepository extends JpaRepository<Sponsorship, Intege
 
 	@Query("select s from Sponsorship s where s.creditCard.id = ?1")
 	Sponsorship findSponsorshipByCreditCardId(Integer creditCardId);
-
-	@Query("select 1.0*count(s)/(select count(ss) from Sponsorship ss ) from Sponsorship s where s.isActive=true")
-	Double ratioActiveSponsorship();
 
 	@Query("select s from Sponsorship s where s.parade.id = ?1")
 	Sponsorship findSponsorshipByParadeId(Integer paradeId);
