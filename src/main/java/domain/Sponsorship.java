92d033a5829f7affe24ad5fa199df233145d6494
danielhingar <danielhingar3397@gmail.com>
2019-03-11 11:36:52 +0100

    Añadidas las entidades Sponsor,Sponsorship y creditcard al domain java

---
+++ src/main/java/domain/Sponsorship.java
@@ -0,0 +1,94 @@
+
+package domain;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Sponsorship extends DomainEntity {
+
+	// Identification ---------------------------------------------------------
+	// ATRIBUTOS
+	private String	banner;
+	private String	target;
+	private boolean	isActive;
+
+
+	@NotBlank
+	@URL
+	public String getBanner() {
+		return this.banner;
+	}
+
+	public void setBanner(final String banner) {
+		this.banner = banner;
+	}
+
+	@NotBlank
+	@URL
+	public String getTarget() {
+		return this.target;
+	}
+
+	public void setTarget(final String target) {
+		this.target = target;
+	}
+
+	public boolean isActive() {
+		return this.isActive;
+	}
+
+	public void setActive(final boolean isActive) {
+		this.isActive = isActive;
+	}
+
+
+	// Relationships ---------------------------------------------------------
+
+	private Sponsor		sponsor;
+	private CreditCard	creditCard;
+	private Procession	procession;
+
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	public Sponsor getSponsor() {
+		return this.sponsor;
+	}
+
+	public void setSponsor(final Sponsor sponsor) {
+		this.sponsor = sponsor;
+	}
+
+	@NotNull
+	@Valid
+	@OneToOne(optional = false)
+	public CreditCard getCreditCard() {
+		return this.creditCard;
+	}
+
+	public void setCreditCard(final CreditCard creditCard) {
+		this.creditCard = creditCard;
+	}
+
+	@Valid
+	@ManyToOne(optional = false)
+	public Procession getProcession() {
+		return this.procession;
+	}
+
+	public void setProcession(final Procession procession) {
+		this.procession = procession;
+	}
+
+}
