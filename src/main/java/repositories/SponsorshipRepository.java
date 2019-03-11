145d6fcd2d528565e238645388606ee15e6e405b
jullopjim1 <julianlopezjimenez@gmail.com>
2019-03-11 17:54:54 +0100

    Algunas queries para sponsorship

--- src/main/java/repositories/SponsorshipRepository.java
+++ src/main/java/repositories/SponsorshipRepository.java
@@ -1,7 +1,10 @@
 
 package repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Sponsorship;
@@ -9,4 +12,11 @@ import domain.Sponsorship;
 @Repository
 public interface SponsorshipRepository extends JpaRepository<Sponsorship, Integer> {
 
+	//Other queries-----------------------------------------------------------------
+
+	@Query("select s from Sponsorship s where s.sponsor.id = ?1")
+	List<Sponsorship> findSponsorshipsBySponsorId(Integer sponsorId);
+
+	@Query("select s from Sponsorship s where s.creditCard.id = ?1")
+	Sponsorship findSponsorshipByCreditCardId(Integer creditCardId);
 }
