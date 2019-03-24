92bbe76a3a9defeba889735ba1568ae3e96cceb2
jullopjim1 <julianlopezjimenez@gmail.com>
2019-03-24 19:48:44 +0100

    caso de uso 16.1 v2

--- src/test/java/usecases/test_v2/UseCase16_1Test.java
+++ src/test/java/usecases/test_v2/UseCase16_1Test.java
@@ -111,6 +111,44 @@ public class UseCase16_1Test extends AbstractTest {
 
 	}
 
+	@Test
+	public void driverDesactivating() {
+		System.out.println("=====DESACTIVATING=====");
+		final Object testingData[][] = {
+			{
+				"sponsor1", null//Sponsor puede editar sus sponsorship (POSITIVO)
+			}, {
+				null, AssertionError.class //Un actor no autenticado no deberia editar sponsorships (NEGATIVO) 
+			}
+		};
+		int j = 1;
+		for (int i = 0; i < testingData.length; i++) {
+			System.out.println("Casuistica" + j);
+			this.templateDesactivating((String) testingData[i][0], (Class<?>) testingData[i][1]);
+			j++;
+		}
+
+	}
+
+	@Test
+	public void driverActivating() {
+		System.out.println("=====ACTIVATING=====");
+		final Object testingData[][] = {
+			{
+				"sponsor1", null//Sponsor puede editar sus sponsorship (POSITIVO)
+			}, {
+				null, AssertionError.class //Un actor no autenticado no deberia editar sponsorships (NEGATIVO) 
+			}
+		};
+		int j = 1;
+		for (int i = 0; i < testingData.length; i++) {
+			System.out.println("Casuistica" + j);
+			this.templateActivating((String) testingData[i][0], (Class<?>) testingData[i][1]);
+			j++;
+		}
+
+	}
+
 	// Ancillary methods ------------------------------------------------------
 
 	protected void templateListing(final String username, final Class<?> expected) {
@@ -193,6 +231,100 @@ public class UseCase16_1Test extends AbstractTest {
 	}
 
 	protected void templateUpdating(final String username, final Class<?> expected) {
+		Class<?> caught;
+		caught = null;
+
+		try {
+
+			//Nos autenticamos
+			this.authenticate(username);
+			final int sponsorId = this.getEntityId(username);
+			final Sponsor principal = this.sponsorService.findOne(sponsorId);
+
+			//Buscamos los sponsorships
+			final List<Sponsorship> sponsorships = this.sponsorshipService.findSponsorshipsBySponsorId(principal.getId());
+
+			//Cogemos y editamos un sponsorship
+			final Sponsorship sponsorship = sponsorships.get(0);
+
+			if (sponsorship.getActive() == true)
+				System.out.println(sponsorship.getBanner() + ", ACTIVATED");
+			else
+				System.out.println(sponsorship.getBanner() + ", DESACTIVATED");
+
+			sponsorship.setActive(false);
+
+			//Gruadamos
+			final Sponsorship saved = this.sponsorshipService.save(sponsorship);
+
+			if (saved.getActive() == true)
+				System.out.println(saved.getBanner() + ", ACTIVATED");
+			else
+				System.out.println(saved.getBanner() + ", DESACTIVATED");
+
+			//Nos desautenticamos
+			this.unauthenticate();
+
+			System.out.println("\n");
+			System.out.println("Editado correctamente.");
+			System.out.println("-----------------------------");
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+
+			System.out.println(caught);
+			System.out.println("-----------------------------");
+		}
+		this.checkExceptions(expected, caught);
+	}
+
+	protected void templateDesactivating(final String username, final Class<?> expected) {
+		Class<?> caught;
+		caught = null;
+
+		try {
+
+			//Nos autenticamos
+			this.authenticate(username);
+			final int sponsorId = this.getEntityId(username);
+			final Sponsor principal = this.sponsorService.findOne(sponsorId);
+
+			//Buscamos los sponsorships
+			final List<Sponsorship> sponsorships = this.sponsorshipService.findSponsorshipsBySponsorId(principal.getId());
+
+			//Cogemos y editamos un sponsorship
+			final Sponsorship sponsorship = sponsorships.get(0);
+
+			if (sponsorship.getActive() == true)
+				System.out.println(sponsorship.getBanner() + ", ACTIVATED");
+			else
+				System.out.println(sponsorship.getBanner() + ", DESACTIVATED");
+
+			sponsorship.setActive(false);
+
+			//Gruadamos
+			final Sponsorship saved = this.sponsorshipService.save(sponsorship);
+
+			if (saved.getActive() == true)
+				System.out.println(saved.getBanner() + ", ACTIVATED");
+			else
+				System.out.println(saved.getBanner() + ", DESACTIVATED");
+
+			//Nos desautenticamos
+			this.unauthenticate();
+
+			System.out.println("\n");
+			System.out.println("Editado correctamente.");
+			System.out.println("-----------------------------");
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+
+			System.out.println(caught);
+			System.out.println("-----------------------------");
+		}
+		this.checkExceptions(expected, caught);
+	}
+
+	protected void templateActivating(final String username, final Class<?> expected) {
 		Class<?> caught;
 		caught = null;
 
