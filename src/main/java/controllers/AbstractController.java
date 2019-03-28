459f559c4faa9abdeccd3eb6c78696a9271a61a5
jullopjim1 <julianlopezjimenez@gmail.com>
2019-03-28 13:13:45 +0100

    updating 404

--- src/main/java/controllers/AbstractController.java
+++ src/main/java/controllers/AbstractController.java
@@ -11,8 +11,10 @@
 package controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.ConfigurationService;
@@ -40,6 +42,7 @@ public class AbstractController {
 	//	}
 
 	@ExceptionHandler(Throwable.class)
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
 	public ModelAndView panic(final Throwable oops) {
 		ModelAndView result;
 
